c427d957f126f0daf724a674d6c8b5dc72f8960f
meannmercolita <167667327+meannmercolita@users.noreply.github.com>
2025-02-18 10:44:53 +0800

    Add files via upload

---
+++ # task4.py
@@ -0,0 +1,13 @@
+sentence = input("Enter a sentence")
+sliced = sentence [:5]
+print(f"Sliced (first 5 characters): {sliced}")
+replaced = sentence.replace('a','@')
+print(f"Replace 'a' with '@': {replaced}")
+uppercase = sentence.upper()
+print(f"Replaced 'a' with '@': {replaced}")
+uppercase = sentence.upper()
+print(f"Uppercase: {uppercase}")
+words = sentence.split()
+print(f"Split words: {words}")
+concatenated = words[0] + words[-1]
+print(f"concatenated first andlast words: {concatenated}")
---
+++ # task5.py
@@ -0,0 +1,20 @@
+# task5.py
+
+class Employee:
+    def __init__(self, name, position, salary):
+        self.name = name
+        self.position = position
+        self.salary = salary
+
+    def give_raise(self, amount):
+        self.salary += amount
+        print(f"Salary increased by ${amount}. New salary: ${self.salary}")
+
+    def display_employee(self):
+        print(f"Employee: {self.name}, Position: {self.position}, Salary: ${self.salary}")
+
+# Create an employee and give a raise
+employee = Employee("John Smith", "Software Engineer", 80000)
+employee.display_employee()
+employee.give_raise(5000)
+employee.display_employee()
